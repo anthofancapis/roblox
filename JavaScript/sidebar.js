08dd99fe7c370248720eaeef321e63152c61be1a
Anthony <tonygeig@gmail.com>
2025-04-22 22:24:55 +0200

    ajout de la sidebar(pas entièrement terminé), correction bug de taille sur mobile et téléphone

---
+++ JavaScript/sidebar.js
@@ -0,0 +1,9 @@
+/*  Sidebar Script */
+
+const sidebarBtn = document.getElementById("sidebar-btn");
+const sidebar = document.getElementById("sidebar");
+
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    console.log("sidebar")
+});  
